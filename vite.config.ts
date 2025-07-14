aa4598738a30b2142b36bf0e3d37ee20003ca87f
mezbahuddindev <mezbahuddin378@gmail.com>
2025-07-14 13:37:09 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/mezbah-vision-forge/",
   server: {
     host: "::",
     port: 8080,
